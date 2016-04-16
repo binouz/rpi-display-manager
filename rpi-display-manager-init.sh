b5397f2a0c778d5e3d037766ce971d0ffb7b4249
Aubin REBILLAT <rebillat.aubin@gmail.com>
2016-04-16 15:41:01 +0200

    Simplified init script

--- rpi-display-manager-init.sh
+++ rpi-display-manager-init.sh
@@ -5,25 +5,6 @@ DAEMON="/usr/bin/rpi-display-manager"
 
 case "$1" in
 	start)
-		params=`grep -Eo 'disp=[^[:blank:]]*' /proc/cmdline`
-		if [ $? -eq 0 ]; then
-			params=`echo $params | cut -d '=' -f2 | sed -e 's/,/ /g'`
-
-			for p in ${params}; do
-				echo "params: ${p}"
-
-				if [ ${p} = "1080p" ]; then
-					echo "Enabling 1080p"
-					export HI379X_DISP_WIDTH=1920
-					export HI379X_DISP_HEIGHT=1080
-				elif [ ${p} = "native" ]; then
-					echo "Enabling sink native mode"
-					export HI379X_DISP_USE_NATIVE=1
-				fi
-			done
-		fi
-
-		killall $NAME
 	        $DAEMON &
 		;;
 
